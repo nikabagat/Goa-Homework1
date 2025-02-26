3053f7612b3adbc319411153d5cbe2a22916d09b
nikabagat <nikolozbagaturia030@gmail.com>
2025-02-27 00:15:15 +0400

    commit

---
+++ Day 64/classwork/script2.js
@@ -0,0 +1,21 @@
+// 2)მომხამრებელს შემოატანინეთ სახელი და გვარი ცალცალკე შემდეგ კი მიესალმეთ
+// var userName = prompt("enter your username: ")
+// var userSurname = prompt("enter your surname: ")
+
+// console.log("hello " + userName + userSurname)
+
+// alert("hello " + userName + userSurname)
+
+//  მომხმარებელს შემოატანინეთ ორი სხვადასხვა რიცხვი შემდეგ კი მომხმარებლის შემოტანილი რიცხვები დაუმატეთ, გაამრავლეთ, გამოაკელით, გაყავით ერთმანეთზე
+
+// var number1 = prompt("enter your number: ")
+// var number2 = prompt("enter your number: ")
+// alert(number1 + number2)
+// alert(number1 - number2)
+// alert(number1 / number2)
+// alert(number1 * number2)
+
+//  მომხმარებელს შემოატანინეთ სახელი და შემდეგ კი ალერტის სახით დაბეჭდეთ წინადადება welcome name
+
+// var userName = prompt("Enter your name: ")
+// alert('Welcome '+ userName)
